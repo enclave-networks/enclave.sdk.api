eed7be60372898c5993f511ccd4bb8a8dc279955
Thomas Soulard <tom.soulard@enclave.io>
2021-12-06 15:02:34 +0000

    Added constructors for generating the IP Ranges

--- src/Enclave.Sdk/Data/EnrolmentKeys/EnrolmentKeyIpConstraintInput.cs
+++ src/Enclave.Sdk/Data/EnrolmentKeys/EnrolmentKeyIpConstraintInput.cs
@@ -1,4 +1,6 @@
-﻿namespace Enclave.Sdk.Api.Data.EnrolmentKeys;
+﻿using System.Net;
+
+namespace Enclave.Sdk.Api.Data.EnrolmentKeys;
 
 /// <summary>
 /// Input model for IP Address constraints.
@@ -6,12 +8,61 @@
 public class EnrolmentKeyIpConstraintInput
 {
     /// <summary>
-    /// A mandatory constructor for creating the IP Constraint.
+    /// Constructor that takes a range as a CIDR notation.
     /// </summary>
-    /// <param name="range">The IP range.</param>
-    public EnrolmentKeyIpConstraintInput(string range)
+    /// <param name="cidrNotation">The IP range as a CIDR notation.</param>
+    public EnrolmentKeyIpConstraintInput(string cidrNotation)
     {
-        Range = range;
+        if (string.IsNullOrWhiteSpace(cidrNotation))
+        {
+            throw new ArgumentNullException(nameof(cidrNotation));
+        }
+
+        if (!cidrNotation.Contains('/', StringComparison.Ordinal))
+        {
+            throw new ArgumentException("Incorrect CIDR Format");
+        }
+
+        if (!IPNetwork.TryParse(cidrNotation, out var network))
+        {
+            throw new ArgumentException("Incorrect CIDR Format");
+        }
+
+        Range = cidrNotation;
+    }
+
+    /// <summary>
+    /// Constructor that takes an IPAddress object to fill the Range field.
+    /// </summary>
+    /// <param name="ipAddress">The IP Address to use as the Range.</param>
+    public EnrolmentKeyIpConstraintInput(IPAddress ipAddress)
+    {
+        if (ipAddress is null)
+        {
+            throw new ArgumentNullException(nameof(ipAddress));
+        }
+
+        Range = ipAddress.ToString();
+    }
+
+    /// <summary>
+    /// Constructor that takes two IPAddress objects to fill the Range property.
+    /// </summary>
+    /// <param name="startAddress">The start IP Address.</param>
+    /// <param name="endAddress">The end IP Address.</param>
+    public EnrolmentKeyIpConstraintInput(IPAddress startAddress, IPAddress endAddress)
+    {
+        if (startAddress is null)
+        {
+            throw new ArgumentNullException(nameof(startAddress));
+        }
+
+        if (endAddress is null)
+        {
+            throw new ArgumentNullException(nameof(endAddress));
+        }
+
+        Range = $"{startAddress} - {endAddress}";
     }
 
     /// <summary>
