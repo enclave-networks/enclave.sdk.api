3742f916e763edac5ec66d95fedecddec896eec1
Thomas Soulard <tom.soulard@enclave.io>
2022-01-27 10:26:15 +0000

    added more xml docs

--- src/Enclave.Sdk/Clients/AuthorityClient.cs
+++ src/Enclave.Sdk/Clients/AuthorityClient.cs
@@ -4,16 +4,23 @@ using Enclave.Sdk.Api.Data.Authority;
 
 namespace Enclave.Sdk.Api.Clients;
 
+/// <inheritdoc cref="IAuthorityClient" />
 internal class AuthorityClient : ClientBase, IAuthorityClient
 {
     private readonly string _orgRoute;
 
+    /// <summary>
+    /// Constructor  which will be called by <see cref="OrganisationClient"/> when it's created.
+    /// </summary>
+    /// <param name="httpClient">an instance of httpClient with a baseURL referencing the API.</param>
+    /// <param name="orgRoute">The organisation API route.</param>
     public AuthorityClient(HttpClient httpClient, string orgRoute)
         : base(httpClient)
     {
         _orgRoute = orgRoute;
     }
 
+    /// <inheritdoc/>
     public async Task<EnrolResult> Enrol(EnrolRequest requestModel)
     {
         if (requestModel is null)
